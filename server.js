0f4d6c97b1e3d1642f9a02979611a11f2f1be1de
LarsA303 <leha20@student.aau.dk>
2021-03-29 09:18:29 +0200

    Lars practice

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const http = require("http");
 
+const fs = require("fs");
+
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h1>Hello Gustav</h1>');
-    res.end();
+    res.writeHead(200, {
+        "Content-Type": "text/html",
+    });
+    fs.readFile("./inputLearningStyle.html", null, function (error, data) {
+        if (error) {
+            res.writeHead(404);
+            res.write("Whoops! File not found!");
+        } else {
+            res.write(data);
+        }
+        res.end();
+    });
 });
 
 const port = process.env.PORT || 3000;
