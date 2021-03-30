98c186bb2183cb7b8c6b023cede5fc7b9a25f25e
MadsNissum <nissum_10@hotmail.com>
2021-03-30 14:19:23 +0200

    Update recommendation.js
    We made some big changes to the recommendation algorithm

--- recommendation.js
+++ recommendation.js
@@ -1,24 +1,87 @@
-function calcProb4(person.undertanding)
+
+let person = {
+    Perception: 11,
+    Input: 1,
+    Processing: -1,
+    Understanding: 11
+};
+
+let lars = {
+    Perception: -3,
+    Input: 5,
+    Processing: -9,
+    Understanding: 7
+};
+
+let rejemond = {
+    Perception: 11,
+    Input: 11,
+    Processing: 11,
+    Understanding: 11
+};
+
+let crazyRaymond = {
+    Perception: 1,
+    Input: 1,
+    Processing: 1,
+    Understanding: 1
+};
+
+let material = {
+    ac: -10,
+    re: -10,
+    se: -10,
+    in: -10,
+    vi: 100,
+    ve: -10,
+    sq: -10,
+    gl: -10
+};
+
+console.log(calcPersonalMaterialScore(person, material));
+
+console.log(calcPersonalMaterialScore(lars, material));
+
+console.log(calcPersonalMaterialScore(rejemond, material));
+
+console.log(calcPersonalMaterialScore(crazyRaymond, material));
 
 
-/*We expect on the database to see how many users, which have the same FS profiles, have voted on a particular material */
-/* */
+/**
+ * @author Mads Overgaard Nissum & Raymond Kacso & Lars Emanuel Hansen
+ * @param {object} person Object from database with the results from the Felder Silverman test
+ * @param {object} material Object from database with the scores from the scoringAlgorithm
+ * @returns Score based on the personal fit for the person viewing the material
+ */
+function calcPersonalMaterialScore(person, material) {
+    let total = Math.abs(person.Perception) + Math.abs(person.Input) + Math.abs(person.Processing) + Math.abs(person.Understanding);
+    let procent1 = (Math.abs(person.Perception) / total);
+    let procent2 = (Math.abs(person.Input) / total);
+    let procent3 = (Math.abs(person.Processing) / total);
+    let procent4 = (Math.abs(person.Understanding) / total);
 
-function {
+    let score1;
+    let score2;
+    let score3;
+    let score4;
 
-}
+    if (person.Perception < 0)
+        score1 = procent1 * material.ac;
+    else
+        score1 = procent1 * material.re;
+    if (person.Input < 0)
+        score2 = procent2 * material.se;
+    else
+        score2 = procent2 * material.in;
+    if (person.Processing < 0)
+        score3 = procent3 * material.vi;
+    else
+        score3 = procent3 * material.ve;
+    if (person.Understanding < 0)
+        score4 = procent4 * material.sq;
+    else
+        score4 = procent4 * material.gl;
+    
 
-function k(material) {
-    let percentage;
-
-    percentage = percentagePositive(material.positive, material.negative);
-
-
-
-}
-
-function percentagePositive(positive, negative) {
-    if (positive === null || negative === null) return;
-
-    return (positive + negative) / positive;
+    return score1 + score2 + score3 + score4;
 }
