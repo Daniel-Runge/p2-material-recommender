9284ca46923839f8b549c000ed1328cf6384023d
MadsNissum <nissum_10@hotmail.com>
2021-03-30 09:34:23 +0200

    Update scoringAlgorithm.js

--- scoringAlgorithm.js
+++ scoringAlgorithm.js
@@ -7,7 +7,7 @@
  * @param {object} person Object with the 4 dimensions from the felder silver test
  * @param {object} material Object with 8 values, 2 values for each of the 4 dimension
  */
-function scoringAlgorithm(vote, person, material) {
+ function scoringAlgorithm(vote, person, material) {
     CalcDim(vote, person.processing, material.reflexive, material.active);
     CalcDim(vote, person.perception, material.intuitive, material.sensing);
     CalcDim(vote, person.input, material.verbal, material.visual);
@@ -50,11 +50,19 @@ function ratingForDimension(value) {
  * @returns {value} returns a score to be added to the material on the opposite side of the dimension where the user has his rating
  */
 function ratingForOppositeDimension(value) {
-
+    
     const result = !value ? Error : (1 / Math.sqrt(Math.abs(value)));
 
     return result;
 }
+
+/* 
+If algorithme dosent work well we can use these two function
+f(x) = 1/10x + 1,4 & f(x) = -1/10x + 1,1 this will make it
+so that if you have 1 in felder silvermantest it will make
+it so that the points added will be 1,5 and 1. If you have 
+11 in the test it will count 2,5.
+*/
 
 module.exports = {
     ratingForDimension,
