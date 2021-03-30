3330802b5cee1a2df8315f7c0c8398b7becd2f01
Raymond <rkacso20student.aau.dk>
2021-03-30 10:26:00 +0200

    Added small changes

---
+++ recommendation.js
@@ -0,0 +1,24 @@
+function calcProb4(person.undertanding)
+
+
+/*We expect on the database to see how many users, which have the same FS profiles, have voted on a particular material */
+/* */
+
+function {
+
+}
+
+function k(material) {
+    let percentage;
+
+    percentage = percentagePositive(material.positive, material.negative);
+
+
+
+}
+
+function percentagePositive(positive, negative) {
+    if (positive === null || negative === null) return;
+
+    return (positive + negative) / positive;
+}
