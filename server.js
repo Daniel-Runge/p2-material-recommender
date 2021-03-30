0c580b967e9c646a274a712bfcfff4e7a209d082
LarsA303 <leha20@student.aau.dk>
2021-03-30 10:15:58 +0200

    made Server.js like on main for merge

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 const http = require("http");
 const port = process.env.PORT || 3000;
 
-const fs = require("fs");
-
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        "Content-Type": "text/html",
-    });
-    fs.readFile("./inputLearningStyle.html", null, function (error, data) {
-        if (error) {
-            res.writeHead(404);
-            res.write("Whoops! File not found!");
-        } else {
-            res.write(data);
-        }
-        res.end();
-    });
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>Hello Gustav</h1>');
+    res.end();
 });
 
 server.listen(port, () => console.log(`Server running at port: ${port} `));
