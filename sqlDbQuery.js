ce8e3758d3d111d88354b9cc7d211eb9e246dfdb
Daniel <dpet20@student.aau.dk>
2021-04-05 15:28:27 +0200

    Small updates to sliders and sliderstest.

--- sqlDbQuery.js
+++ sqlDbQuery.js
@@ -1,39 +1,30 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
-
-function sqlConstructorSignUp (signUpData){
+function sqlConstructorSignUp(signUpData) {
   console.log("sidgn up ", signUpData.value.username);
   const sql = `INSERT INTO users (Email, UserPassword)
-  VALUES ("${signUpData.value.username}","${signUpData.value.password}")`
+  VALUES ("${signUpData.value.username}","${signUpData.value.password}")`;
   queryToSqlDb(sql);
 }
 
-function queryToSqlDb (sqlquery){
-    const con = mysql.createConnection({
-<<<<<<< HEAD
-        host: "localhost",
-        user: "g",
-        password: "123456",
-        database: "db19"
-=======
-        host: process.env.DATABASE_HOST || "localhost",
-        user: process.env.DATABASE_USER || "root",
-        password: process.env.DATABASE_PASSWORD || "",
-        database: process.env.DATABASE || "database"
->>>>>>> fc9108a53493d878be3ea83592f39d16d02d3205
-      });
-      
-    con.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected!");
-        console.log(sqlquery);
-        con.query(sqlquery, function (err, result) {
-            if (err) throw err;
-                console.log("1 record inserted");
-                console.log(result);
-          });
-        });
+function queryToSqlDb(sqlquery) {
+  const con = mysql.createConnection({
+    host: process.env.DATABASE_HOST || "localhost",
+    user: process.env.DATABASE_USER || "root",
+    password: process.env.DATABASE_PASSWORD || "",
+    database: process.env.DATABASE || "database",
+  });
+
+  con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+    console.log(sqlquery);
+    con.query(sqlquery, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+      console.log(result);
+    });
+  });
 }
 
-module.exports = { sqlConstructorSignUp }
-
+module.exports = { sqlConstructorSignUp };
--- tests/Sliders.test.js
+++ tests/Sliders.test.js
@@ -1,46 +1,41 @@
-const { CreateLearningStyleSliders } = require("../pages/util/Sliders");
+const { createLearningStyleSliders } = require("../pages/util/Sliders");
 
 describe("The function to generate four sliders for input", () => {
     test("Works without input", () => {
-        const actual = CreateLearningStyleSliders();
+        const actual = createLearningStyleSliders();
         expect(actual).toBe(`
-<div class ="container">
-    <h1>Profile</h1>
-    <p>Felder Silverman results</p>
-    <form action="/style" method="POST">
+<div class ="sliders-container">
+    <h3>Felder Silverman results</h3>
+    <form class="sliders" action="/style" method="POST">
         <label>Perception</label><br>
         <input type="range" id="Perception" max="11" min="-11" value="0" step="2"
-            onchange="updateTextInput(this.value, 'PerceptionVal');"><br>
-        <input type="text" id="PerceptionVal" value="0"><br><br>
+            onchange="updateTextInput(this.value, 'PerceptionVal');">
+        <input type="text" id="PerceptionVal" value="0" class="untargetable">
 
         <label>Input</label><br>
         <input type="range" id="Input" max="11" min="-11" value="0" step="2"
-            onchange="updateTextInput(this.value, 'InputVal');"><br>
-        <input type="text" id="InputVal" value="0"><br><br>
+            onchange="updateTextInput(this.value, 'InputVal');">
+        <input type="text" id="InputVal" value="0" class="untargetable">
 
         <label>Processing</label><br>
         <input type="range" id="Processing" max="11" min="-11" value="0" step="2"
-            onchange="updateTextInput(this.value, 'ProcessingVal' );"><br>
-        <input type="text" id="ProcessingVal" value="0"><br><br>
+            onchange="updateTextInput(this.value, 'ProcessingVal' );">
+        <input type="text" id="ProcessingVal" value="0" class="untargetable">
 
         <label>Understanding</label><br>
         <input type="range" id="Understanding" max="11" min="-11" value=0 step="2"
-            onchange="updateTextInput(this.value, 'UnderstandingVal');"><br>
-        <input type="text" id="UnderstandingVal" value="0"><br><br>
+            onchange="updateTextInput(this.value, 'UnderstandingVal');">
+        <input type="text" id="UnderstandingVal" value="0" class="untargetable">
 
-        <input type="submit" value="Submit"><br>
+        <input type="submit" value="Update">
     </form>
- <button onclick="myfunction()" id="button">
-            button 1
-        </button>
+
 </div>
 <script>
     function updateTextInput(val, id) {
         document.getElementById(id).value = val;
         console.log("The value of " + id + " is: " + val);
     }
-</script>
-
-</html>`);
+</script>`);
     });
 })
