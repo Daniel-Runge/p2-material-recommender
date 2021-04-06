8d2e45fd51ff56e125c74b30c6fb76229277d821
Raymond <rkacso20student.aau.dk>
2021-04-06 09:12:37 +0200

    Local fix

--- sqlDbQuery.js
+++ sqlDbQuery.js
@@ -1,30 +1,30 @@
 const mysql = require("mysql");
 
 function sqlConstructorSignUp(signUpData) {
-  console.log("sidgn up ", signUpData.value.username);
-  const sql = `INSERT INTO users (Email, UserPassword)
+    console.log("sidgn up ", signUpData.value.username);
+    const sql = `INSERT INTO users (Email, UserPassword)
   VALUES ("${signUpData.value.username}","${signUpData.value.password}")`;
-  queryToSqlDb(sql);
+    queryToSqlDb(sql);
 }
 
 function queryToSqlDb(sqlquery) {
-  const con = mysql.createConnection({
-    host: process.env.DATABASE_HOST || "localhost",
-    user: process.env.DATABASE_USER || "root",
-    password: process.env.DATABASE_PASSWORD || "",
-    database: process.env.DATABASE || "database",
-  });
-
-  con.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected!");
-    console.log(sqlquery);
-    con.query(sqlquery, function (err, result) {
-      if (err) throw err;
-      console.log("1 record inserted");
-      console.log(result);
+    const con = mysql.createConnection({
+        host: process.env.DATABASE_HOST || "localhost",
+        user: process.env.DATABASE_USER || "root",
+        password: process.env.DATABASE_PASSWORD || "",
+        database: process.env.DATABASE || "database",
     });
-  });
+
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        console.log(sqlquery);
+        con.query(sqlquery, function(err, result) {
+            if (err) throw err;
+            console.log("1 record inserted");
+            console.log(result);
+        });
+    });
 }
 
 module.exports = { sqlConstructorSignUp };
