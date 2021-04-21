1d3104346b081295f0fffc63e020b24ec0c3aedf
Raymond <rkacso20student.aau.dk>
2021-04-21 14:29:21 +0200

    Added jwt functionalities and their tests. Added changes in the profilePage and processRequest. Website.js has a new function login

--- jwtLogin.js
+++ jwtLogin.js
@@ -1,81 +1,30 @@
 const jwt = require('jsonwebtoken');
 
-//dummy data
-const user = {
-    id: 1,
-    code: 'secret',
-    name: 'dummy'
-};
-
-const user2 = {
-    id: 2,
-    code: 'wow',
-    name: 'dummy-Jr'
-};
-
 /**
- * 
- * @param {user id} the request from the user 
+ * @author Daniel Runge and Raymond Kacso
+ * @param {Int} id user id to generate tokens 
  * @returns a long string with  the token headers, payload and signature separated by dots
  */
-function tokenCreater(id) {
-    const token = jwt.sign({ id }, `${process.env.JWT_SECRET}`, {
-        expiresIn: 24
-    }, {
-        httpOnly: true
-    });
+function createToken(id) {
+    const token = jwt.sign({ id }, `${process.env.JWT_SECRET}`);
     return token;
 }
 
-//an example of the function
-console.log("The first user is:    " + tokenCreater(user));
-console.log();
-console.log("The second user is:   " + tokenCreater(user2));
-
-
 
 /**
- * 
- * @param {token} the token that is created by the function createToken
- * @return undefined??? 
+ * @author Daniel Runge and Raymond Kacso
+ * @param {String} token that is created by the function createToken
+ * @return false if the token is not present. Else it returns an object
  */
-function tokenVerifier(token) {
-    const jwtDecoder = jwt.verify(tokenCreater(token), `${process.env.JWT_SECRET}`, (err, decoded) => {
-        if (err) {
-            console.log(err);
-            // should redirect to the loginPage
-        }
+function verifyToken(token) {
+    if (!token) return false;
 
-
-        //else is optional - it shows the decoded form of the token
-        else {
-            console.log(decoded);
-
-        }
+    const jwtDecoder = jwt.verify(token, `${process.env.JWT_SECRET}`, (err, decoded) => {
+        if (err) return false;
+        
+        return decoded;
     });
-
+    return jwtDecoder;
 }
 
-//this function should send the token to the client side
-function tokenToCookie(token) {
-    console.log("");
-}
-
-//this function should read the cookie from the client side
-function cookieReader(token) {
-    console.log("");
-}
-
-
-/**
- * @ThisNotSoImportantFunctionIsAPrototype
- * @param {user id} the request from the user.
- *          Not done
- */
-function tokenRefresher(id) {
-    const refresh = jwt.sign({ id }, tokenVersion, `${process.env.JWT_TOKEN_REFRESH}`, {
-        expiresIn: 24 * 60 * 60 * 3
-    });
-    return refresh;
-}
-console.log(tokenVerifier(user));
+module.exports = { createToken, verifyToken };
--- website.js
+++ website.js
@@ -3,89 +3,105 @@ const { loginhtml } = require("./pages/loginhtml");
 const { signuphtml } = require("./pages/signuphtml");
 const { profilehtml } = require("./pages/profilehtml");
 const { sqlConstructorSignUp, queryToSqlDb } = require("./sqlDbQuery")
-
+const { createToken, verifyToken } = require('./jwtLogin');
 
 
 class Website {
-  title;
-  csss;
-  scripts;
-  header;
-  constructor(title, csss = [], scripts = []) {
-    this.title = title;
-    this.csss = csss;
-    this.scripts = scripts;
-    this.header = htmlHeader(title, csss, scripts);
-  }
+    title;
+    csss;
+    scripts;
+    header;
+    constructor(title, csss = [], scripts = []) {
+        this.title = title;
+        this.csss = csss;
+        this.scripts = scripts;
+        this.header = htmlHeader(title, csss, scripts);
+    }
 
-  // Getters
-  getTitle() {
-    return this.title;
-  }
-  getCsss() {
-    return this.csss;
-  }
-  getScripts() {
-    return this.scripts;
-  }
+    // Getters
+    getTitle() {
+        return this.title;
+    }
+    getCsss() {
+        return this.csss;
+    }
+    getScripts() {
+        return this.scripts;
+    }
 
-  // Methods
-  //   home(res) {
-  //     // This method will return a logged in user to their profile page, and others to the login page
-  //     return loginPage(res);
-  //   }
+    // Methods
+    //   home(res) {
+    //     // This method will return a logged in user to their profile page, and others to the login page
+    //     return loginPage(res);
+    //   }
 
-  loginPage(res) {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.write(this.header + loginhtml());
-    res.end();
-  }
+    loginPage(res) {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.write(this.header + loginhtml());
+        res.end();
+    }
 
-  signupPage(res) {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.write(this.header + signuphtml());
-    res.end();
-  }
+    signupPage(res) {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.write(this.header + signuphtml());
+        res.end();
+    }
 
-  profilePage(res) {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.write(this.header + profilehtml());
-    res.end();
-  }
+    profilePage(res, token) {
+        if (!verifyToken(token)) {
+            res.writeHead(301, {location: '/login'});
+            res.end();
+        }
 
-  signup(req, res) {
-    //placeholder
-    // let signUpObject;
-     res.setHeader("Content-Type", "application/json");
-     let data = "";
-       req.on("data", (chunk) => {
-       data += chunk
-       console.log(data);
-      });
-     req.on("end", () => {
-    //   console.log("server gets", JSON.parse(data))
-    //   signUpObject = JSON.parse(data);
-    //   console.log("works until sqlconstructor");
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.write(this.header + profilehtml());
+        res.end();
+    }
 
-    //   const sqlQuery = sqlConstructorSignUp(signUpObject)
-    //   console.log('query =', sqlQuery);
-      
-    //   queryToSqlDb(sqlQuery, (result) => console.log(`the arrow is good ${result}`))
+    signup(req, res) {
+        //placeholder
+        // let signUpObject;
+        res.setHeader("Content-Type", "application/json");
+        let data = "";
+        req.on("data", (chunk) => {
+            data += chunk
+            console.log(data);
+        });
+        req.on("end", () => {
+            //   console.log("server gets", JSON.parse(data))
+            //   signUpObject = JSON.parse(data);
+            //   console.log("works until sqlconstructor");
 
-      queryToSqlDb('SELECT * FROM Users', (result) => result.map(
-        (row) => console.log(row.Email)
-        )
-      )
+            //   const sqlQuery = sqlConstructorSignUp(signUpObject)
+            //   console.log('query =', sqlQuery);
 
-       res.end();
-     });
-    
+            //   queryToSqlDb(sqlQuery, (result) => console.log(`the arrow is good ${result}`))
 
+            queryToSqlDb('SELECT * FROM Users', (result) => result.map(
+                (row) => console.log(row.Email)
+            ))
 
-  }
+            res.end();
+        });
+
+    }
+
+    login(req, res) {
+        //implementer senere; authentication
+
+        let id = 1;
+        const token = createToken(id);
+        res.writeHead(200, {
+            'Set-Cookie': 'authCookie=' + token,
+             httpOnly: true,
+            'Content-Type': 'text/html'
+        });
+        res.write("<h1>TEST: Login Succesful</h1>");
+        res.end();
+    }
 }
 
 module.exports = { Website };
